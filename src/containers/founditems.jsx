74aa513bf264b59e863d038571c75725cc4ae276
Cavlovic <jcavlovic91@gmail.com>
2024-09-04 21:56:21 -0500

    Once again

---
+++ src/containers/founditems.jsx
@@ -0,0 +1,104 @@
+const itemsFound = [
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+  {
+    item: "Life Raft",
+    time: "00:00:00",
+    location: "./src/assests/Life_Raft.png",
+  },
+];
+
+export default itemsFound;
---
+++ src/containers/timeline.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const timeline = () => {
+  const images = ["an", "array", "of", "images"];
+  return (
+    //create timeline
+    <div>
+      <h1>Timeline</h1>
+    </div>
+  );
+};
+
+const timelineImages = (image) => {
+  <div></div>;
+};
