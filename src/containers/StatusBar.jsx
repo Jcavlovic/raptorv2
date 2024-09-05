c7f60d7db5f75ded24c7aa2603a374ac544fd61b
Cavlovic <jcavlovic91@gmail.com>
2024-09-04 21:55:32 -0500

    Lots of edits

--- src/containers/StatusBar.jsx
+++ src/containers/StatusBar.jsx
@@ -1,33 +1,19 @@
 import React from "react";
+import itemsFound from "./founditems";
 
 const StatusBar = () => {
-  const itemsFound = [
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-    { item: "Life Raft", time: "00:00:00" },
-  ];
+  const items = itemsFound;
   return (
     <div className="statusbar">
       <h1 className="statusbarTitle"> &ensp;Found Objects</h1>
-      {itemsFound.map((itemFound) => (
-        <li className="foundlist">
+      {itemsFound.map((itemFound, index) => (
+        <li
+          key={`${itemFound}${index}`}
+          className="foundlist"
+          onClick={() => {
+            alert(`You clicked ${itemFound.item}`);
+          }}
+        >
           {" "}
           {itemFound.item} {itemFound.time}
         </li>
