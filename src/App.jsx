8d607ea1f46dc5cc5e11b0f2fa2125b8ddc69f29
Cavlovic <jcavlovic91@gmail.com>
2024-09-07 22:22:12 -0500

    Removed unnessacary app.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./App.css";
 import ToggleSwitches from "./containers/ToggleSwitches.jsx";
 import MenuBar from "./containers/MenuBar.jsx";
 import StatusBar from "./containers/StatusBar.jsx";
