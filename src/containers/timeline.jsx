0678e7bdb163d89296134b44ca523d9de50515b0
Cavlovic <jcavlovic91@gmail.com>
2024-09-07 22:17:52 -0500

    pass

--- src/containers/timeline.jsx
+++ src/containers/timeline.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+/* 
+  https://www.shecodes.io/athena/38751-how-to-import-all-images-of-a-folder-into-a-react-component 
+  Use this website to help create the timeline.
+*/
+
 const timeline = () => {
   const images = ["an", "array", "of", "images"];
   return (
