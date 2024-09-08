bf9c914db5ea3a93ab4e9e26ca3193f5ed731fc5
Cavlovic <jcavlovic91@gmail.com>
2024-09-08 14:48:32 -0500

    afk

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ToggleSwitches from "./containers/ToggleSwitches.jsx";
 import MenuBar from "./containers/MenuBar.jsx";
 import StatusBar from "./containers/StatusBar.jsx";
+//import ImageGallary from "./containers/timeline.jsx";
 
 function App() {
   return (
--- src/containers/timeline.jsx
+++ src/containers/timeline.jsx
@@ -1,20 +1,22 @@
 import React from "react";
 
-/* 
-  https://www.shecodes.io/athena/38751-how-to-import-all-images-of-a-folder-into-a-react-component 
+/*
+  https://www.shecodes.io/athena/38751-how-to-import-all-images-of-a-folder-into-a-react-component
   Use this website to help create the timeline.
 */
 
-const timeline = () => {
-  const images = ["an", "array", "of", "images"];
+const images = require.context("../assets", true);
+
+const imageList = images.keys().map((image) => images(image));
+
+function ImageGallary() {
   return (
-    //create timeline
     <div>
-      <h1>Timeline</h1>
+      {imageList.map((image, index) => (
+        <img key={index} src={image.default} alt={`image-${index}`} />
+      ))}
     </div>
   );
-};
+}
 
-const timelineImages = (image) => {
-  <div></div>;
-};
+export default ImageGallary;
