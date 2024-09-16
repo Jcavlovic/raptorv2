a7661760ea30e7810c491a5d24bcab394e508721
Cavlovic <jcavlovic91@gmail.com>
2024-09-15 20:01:18 -0500

    removed old code

--- backend/api.py
+++ backend/api.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 from typing import List
 from pydantic import BaseModel
 import os
-from detection import foundObjectDir
+from raptorv2.backend.detection.detection import foundObjectDir
 from fastapi.middleware.cors import CORSMiddleware
 
 class SwitchState(BaseModel):
@@ -55,24 +55,4 @@ async def set_switches(state: SwitchState):
     print(switch_states)
     return switch_states
 
-# @app.get("/images", response_model=List[str])
-# async def images():
-    
 
-#     image_files = [f for f in os.listdir('/home/raptor/Documents/raptor/static/images/Library/Detections') if f.endswith(('.JPG', '.jpg'))]
-#     return image_files
-
-# @app.get("/images/{filename}")
-# async def image(filename: str):
-#     file_path = f"/home/raptor/Documents/raptor/static/images/Library/Detections/{filename}"
-#     return FileResponse(path=file_path)
-
-# @app.get("/library_images", response_model=List[str])
-# async def images_library(category: List[str]):
-#     image_paths = []
-#     for cat in category:
-#         image_dir = foundObjectDir.get(cat)
-#         if image_dir:
-#             image_files = [f for f in os.listdir(image_dir) if os.path.isfile(os.path.join(image_dir, f))]
-#             image_paths.extend([os.path.join(image_dir, file) for file in image_files])
-#     return image_paths
